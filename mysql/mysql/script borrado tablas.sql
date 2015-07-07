7d5bfc9328f93697dc2061b1eaa34b1a86adb1dd
Franciso de Borja Omaña <fbomana@gmail.com>
2015-07-07 16:27:15 +0200

    Eliminamos el submodulo

---
+++ mysql/mysql/script borrado tablas.sql
@@ -0,0 +1,11 @@
+delete from GENERATOR_TABLE;
+delete from historico_proyectos;
+delete from acciones_historico;
+delete from tareas;
+delete from proyectos;
+delete from estado_proyectos;
+delete from roles_usuario;
+delete from permisos_roles;
+delete from roles;
+delete from permisos;
+delete from usuarios;
